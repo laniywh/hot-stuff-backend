0c3f09da10818ec29c0ccbf09b5fdf9268f9e415
Lani Huang <lani.huang@capitalbrands.com>
2019-09-24 11:12:36 -0700

    initial commit

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// Enable access to remote prisma DB with JS
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
