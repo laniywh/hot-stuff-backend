6098727eec57c2c34b308c0784d5fa411c08ce1b
laniywh <laniywh@gmail.com>
2019-10-07 16:59:00 -0700

    Add properties to type User

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -17,6 +20,7 @@ const Mutations = {
   updateItem(parent, args, ctx, info) {
     const updates = { ...args };
     delete updates.id;
+    j;
     return ctx.db.mutation.updateItem(
       {
         data: updates,
@@ -35,6 +39,29 @@ const Mutations = {
     // TODO
     // delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase email
+    args.email = args.email.toLowerCase();
+    // hash password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in the database
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ["USER"] } // need to set it b/c it's enum
+      }
+    });
+    // create JWT token for the user
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the JWT as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return user to the browser
+    return user;
   }
 };
 
